ebe38de65a9a37106fbf2ebde87431aac53973ab
21996193 <grace@Ubuntu.20.4.myguest.virtualbox.org>
2025-10-30 19:10:37 +0800

    Updated HomePage

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Student ID is 21996193. Welcome to Express.' });
+  res.render('index', { title: 'title' });
 });
 
 export default router;
